5f81488eaa6e7b552ab5664dcb0ce46487afe1dc
Pratham Sharma <47818166+prathamsh19@users.noreply.github.com>
2020-01-23 05:25:26 +0530

    Add files via upload

---
+++ Pratham3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Pratham3
+{
+  public static void main(String args[])
+  {
+     Scanner S = new Scanner (System.in);
+     String St = S.nextLine();
+     System.out.println(St);
+     int a = S.nextInt();
+     System.out.println(a);
+  }
+}
