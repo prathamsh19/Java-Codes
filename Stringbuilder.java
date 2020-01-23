5f93554f076bfc0db8be32db1b8da0ff66d9e852
Pratham Sharma <47818166+prathamsh19@users.noreply.github.com>
2020-01-23 05:56:03 +0530

    Add files via upload

---
+++ Stringbuilder.java
@@ -0,0 +1,11 @@
+class Stringbuilder
+{
+  public static void main(String args[])
+  {
+       StringBuilder sb = new StringBuilder();
+       sb.append("ABC");
+       System.out.println(sb);
+       sb.append("EFG");
+       System.out.println(sb);
+  }
+}
